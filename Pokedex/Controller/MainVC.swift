8e9389bf35f6b200453a2e3aa620ec6a1a7ac8b2
Shravan Chopra <shrav_20@hotmail.com>
2017-12-10 21:17:46 -0600

    Implemented search filtering using search bar

--- Pokedex/Controller/MainVC.swift
+++ Pokedex/Controller/MainVC.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     // to store and display pokemon data
     var pokemon: [Pokemon]!
     @IBOutlet weak var pokeCollectionView: UICollectionView!
 
+    // to make pokemon data searchable
+    @IBOutlet weak var searchBar: UISearchBar!
+    var filteredPokemon: [Pokemon]!
+    var inSearchMode = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,10 +26,11 @@ class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         startUp()
     }
 
-    // Function: hooks up collection view to this VC, and loads pokemon data from csv file
+    // Function: hooks up collection view to this VC, sets up delegates, and loads data from csv file
     func startUp() {
         pokeCollectionView.dataSource = self
         pokeCollectionView.delegate = self
+        searchBar.delegate = self
         pokemon = loadPokemonData(fileName: "pokemon")
     }
     
@@ -66,6 +72,11 @@ class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+        
         return pokemon.count
     }
     
@@ -73,7 +84,13 @@ class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         
         if let pokeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemonCell", for: indexPath) as? PokemonCell{
             
-            pokeCell.updateUI(from: pokemon[indexPath.row])
+            if inSearchMode {
+                pokeCell.updateUI(from: filteredPokemon[indexPath.row])
+            }
+            else {
+                pokeCell.updateUI(from: pokemon[indexPath.row])
+            }
+            
             return pokeCell
         }
         
@@ -85,8 +102,21 @@ class MainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         return CGSize(width: 110, height: 110)
     }
     
-    // ---------------------------------------------------------------------------------------------
-    
+    // --------------------- Search bar function to implement search-filtering -------------------
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            pokeCollectionView.reloadData()
+        }
+        else {
+            inSearchMode = true
+            let pokeLowered = searchBar.text!.lowercased()
+            filteredPokemon = pokemon.filter( {$0.name.range(of: pokeLowered) != nil} )
+            
+            pokeCollectionView.reloadData()
+        }
+    }
 
 }
 
