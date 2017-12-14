91a41c85625b0a1c43f906a40aa3f4b589d90695
Shravan Chopra <shrav_20@hotmail.com>
2017-12-14 15:06:04 -0600

    Added a utility file to store base URL's  to make API calls

---
+++ Pokedex/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Shravan Chopra on 14/12/17.
+//  Copyright © 2017 Shravan Chopra. All rights reserved.
+//
+
+import Foundation
+
+let POKE_ATTRIBUTES_DICT = [
+    0: "Type",
+    1: "HP",
+    2: "Height",
+    3: "Attack",
+    4: "Weight",
+    5: "Defense"
+]
+
+// TODO - add base URL's for normal and evolution stats to make API calls!
